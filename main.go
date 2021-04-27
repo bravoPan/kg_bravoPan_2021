9ea717cb93699b34cc8aa1325644916befe4b728
Pan <dengpan.yuan.cool@gmail.com>
2021-04-26 20:43:04 -0400

    Update main.go
    Finish read by Args and Convert

--- main.go
+++ main.go
@@ -2,42 +2,42 @@ package main
 
 import (
   "fmt"
-  "math"
   "strings"
   "os"
 )
 
-var pho_map map[int]string
+var pho_map map[string]string
 
-func convert(num int){
-  var digit_len = int(math.Log10(float64(num))) + 1
-  str_arr := make([]string, digit_len)
-  for i:=0; i < digit_len; i++{
-    str_arr[digit_len - 1 - i] = pho_map[num % 10]
-    num /= 10
+func convert(str_num string){
+  str_arr := make([]string, len(str_num))
+  for i:=0; i < len(str_num); i++{
+    str_arr[i] = pho_map[string(str_num[i])]
   }
-  fmt.Println(strings.Join(str_arr[:], ""))
+
+  fmt.Print(strings.Join(str_arr[:], ""))
 }
 
 func main(){
 
-  pho_map = make(map[int]string)
+  pho_map = make(map[string]string)
 
-  pho_map[0] = "Zero"
-  pho_map[1] = "One"
-  pho_map[2] = "Two"
-  pho_map[3] = "Three"
-  pho_map[4] = "Four"
-  pho_map[5] = "Five"
-  pho_map[6] = "Six"
-  pho_map[7] = "Seven"
-  pho_map[8] = "Eight"
-  pho_map[9] = "Nine"
+  pho_map["0"] = "Zero"
+  pho_map["1"] = "One"
+  pho_map["2"] = "Two"
+  pho_map["3"] = "Three"
+  pho_map["4"] = "Four"
+  pho_map["5"] = "Five"
+  pho_map["6"] = "Six"
+  pho_map["7"] = "Seven"
+  pho_map["8"] = "Eight"
+  pho_map["9"] = "Nine"
 
-  // for i:=0; i < len(os.Args); i++{
-  //   convert(os.Args[i])
-  // }
+  argsWithoutProg := os.Args[1:]
+  for i:=0; i < len(argsWithoutProg); i++{
+    convert(argsWithoutProg[i])
+    if i != len(argsWithoutProg) - 1{
+      fmt.Print(",")
+    }
+  }
 
-  // convert(45456756786785)
-  // fmt.Println(5)
 }
